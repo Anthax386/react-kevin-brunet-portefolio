be641035e94faab8a04e08329a4f3fc8e5de8ff7
Anthax386 <kevinanthax@protonmail.com>
2024-02-05 00:30:19 +0100

    edit error messages

--- backend/controllers/lang.controller.js
+++ backend/controllers/lang.controller.js
@@ -2,51 +2,39 @@ const LangModel = require('../models/lang.model');
 
 module.exports.getLang = async (req, res) => {
     const lang = await LangModel.find();
-    res.status(200).json(lang)
-}
+    res.status(200).json(lang);
+};
 
 module.exports.setLang = async (req, res) => {
-    if (!req.body.name) {
-        res.status(400).json({ message : "Merci d'ajouter un nom."})
-    }
-    if (!req.body.image) {
-        res.status(400).json({ message : "Merci d'ajouter une image."})
-    }
-
+    if (!req.body.name || !req.body.image) {
+        res.status(400).json({ message : "Missing datas"});
+    };
     const lang = await LangModel.create({
         name: req.body.name,
         image: req.body.image,
         display: req.body.display
-    })
-
-    res.status(200).json(lang)
-}
+    });
+    res.status(200).json(lang);
+};
 
 module.exports.editLang = async (req, res) => {
-    const lang = await LangModel.findById(req.params.id)
-
+    const lang = await LangModel.findById(req.params.id);
     if (!lang) {
-        res.status(400).json({message : "Ce langage n'existe pas."})
-    }
-
+        res.status(400).json({message : "404 : lang not found"});
+    };
     const updateLang = await LangModel.findByIdAndUpdate(
         lang,
         req.body,
         {new: true},
-    )
-
-    res.status(200).json(updateLang)
-}
+    );
+    res.status(200).json(updateLang);
+};
 
 module.exports.deletLang = async (req, res) => {
-
     const lang = await LangModel.findById(req.params.id);
-
     if (!lang) {
-        res.status(400).json({ message: "Ce langage n'existe pas." });
-    }
-
-    await lang.deleteOne({ _id: lang })
-
-    res.status(200).json("Langage supprimé " + req.params.id);
-}
+        res.status(400).json({ message: "404 : lang not found" });
+    };
+    await lang.deleteOne({ _id: lang });
+    res.status(200).json("Lang deleted " + req.params.id);
+};
--- backend/controllers/tool.contoller.js
+++ backend/controllers/tool.contoller.js
@@ -1,52 +1,40 @@
-const ToolModel = require('../models/tool.model')
+const ToolModel = require('../models/tool.model');
 
 module.exports.getTool = async (req, res) => {
     const tool = await ToolModel.find();
-    res.status(200).json(tool)
-}
+    res.status(200).json(tool);
+};
 
 module.exports.setTool = async (req, res) => {
-    if (!req.body.name) {
-        res.status(400).json({ message : "Merci d'ajouter un nom."})
-    }
-    if (!req.body.image) {
-        res.status(400).json({ message : "Merci d'ajouter une image."})
-    }
-
+    if (!req.body.name || !req.body.image) {
+        res.status(400).json({ message : "Missing datas"});
+    };
     const tool = await ToolModel.create({
         name: req.body.name,
         image: req.body.image,
         display: req.body.display,
     });
-
-    res.status(200).json(tool)
-}
+    res.status(200).json(tool);
+};
 
 module.exports.editTool = async (req, res) => {
-    const tool = await ToolModel.findById(req.params.id)
-
+    const tool = await ToolModel.findById(req.params.id);
     if (!tool) {
-        res.status(400).json({message : "Ce tool n'existe pas."})
-    }
-
+        res.status(400).json({message : "404 : tool not found"});
+    };
     const updateTool = await ToolModel.findByIdAndUpdate(
         tool,
         req.body,
         {new: true},
-    )
-
-    res.status(200).json(updateTool)
-}
+    );
+    res.status(200).json(updateTool);
+};
 
 module.exports.deletTool = async (req, res) => {
-
     const tool = await ToolModel.findById(req.params.id);
-
     if (!tool) {
-        res.status(400).json({ message: "Ce tool n'existe pas." });
-    }
-
-    await tool.deleteOne({ _id: tool })
-
+        res.status(400).json({ message: "404 : tool not found" });
+    };
+    await tool.deleteOne({ _id: tool });
     res.status(200).json("tool supprimé " + req.params.id);
-}
+};
