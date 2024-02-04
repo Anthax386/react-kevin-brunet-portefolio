368cf4fb41e2fbf22b74cd4923bd314cbab8f4dc
Anthax386 <kevinanthax@protonmail.com>
2024-02-05 00:30:00 +0100

    edit data verification

--- backend/controllers/work.controller.js
+++ backend/controllers/work.controller.js
@@ -6,22 +6,9 @@ module.exports.getWorks = async (req, res) => {
 }
 
 module.exports.setWork = async (req, res) => {
-    if (!req.body.name) {
-        res.status(400).json({ message : "Merci d'ajouter un nom."});
+    if (!req.body.name || !req.body.readMe || !req.body.preview || !req.body.githubLink || !req.body.preview) {
+        res.status(400).json({ message : "Missing datas"});
     };
-    if (!req.body.readMe) {
-        res.status(400).json({ message : "Merci d'ajouter une description."});
-    };
-    if (!req.body.preview) {
-        res.status(400).json({ message : "Merci d'ajouter une image."});
-    };
-    if (!req.body.githubLink) {
-        res.status(400).json({ message : "Merci d'ajouter un lien du repo Github."});
-    };
-    if (!req.body.preview) {
-        res.status(400).json({ message : "Merci d'ajouter une image."});
-    };
-
     const work = await WorkModel.create({
         name: req.body.name,
         readMe: req.body.readMe,
@@ -31,35 +18,27 @@ module.exports.setWork = async (req, res) => {
         tags: req.body.tags,
         display: req.body.display
     });
-
     res.status(200).json(work);
-}
+};
 
 module.exports.editWork = async (req, res) => {
     const work = await WorkModel.findById(req.params.id);
-
     if (!work) {
-        res.status(400).json({message : "Ce work n'existe pas."});
+        res.status(400).json({message : "404 : work not found"});
     };
-
     const updateWork = await WorkModel.findByIdAndUpdate(
         work,
         req.body,
         {new: true},
     );
-
     res.status(200).json(updateWork);
-}
+};
 
 module.exports.deletWork = async (req, res) => {
-
     const work = await WorkModel.findById(req.params.id);
-
     if (!work) {
-        res.status(400).json({ message: "Ce work n'existe pas" });
-    }
-
+        res.status(400).json({ message: "404 : work not found" });
+    };
     await work.deleteOne({ _id: work });
-
     res.status(200).json("Work supprimé " + req.params.id);
-}
+};
